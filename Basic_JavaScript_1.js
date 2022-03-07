cad925ea9eecc7438b9a1e10e8c9019f51fcc4b0
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-03-07 13:42:51 -0800

    Submission Assignment 2

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+//alert("Hello World!")
+
+function My_First_Function() {
+	var string = "Kiss me, I'm Irish!"
+	var result = string.fontcolor("green")
+	console.log (result)
+	document.getElementById("Irish").innerHTML = result
+}
